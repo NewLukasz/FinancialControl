03c93b7b4205f82760ff1cedeac6d810de2e9e3c
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2020-11-02 17:14:28 +0100

    Project init

---
+++ FinancialControlMM.mm
@@ -0,0 +1,99 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1604089782440" ID="ID_156533242" MODIFIED="1604090127720" TEXT="FinacialControl">
+<node CREATED="1604090163880" ID="ID_1023545014" MODIFIED="1604090169388" POSITION="right" TEXT="Main menu">
+<node CREATED="1604090190091" ID="ID_466698915" MODIFIED="1604090563074" TEXT="1. Login">
+<node CREATED="1604090435600" ID="ID_628993872" MODIFIED="1604090448975" TEXT="1. Type login">
+<node CREATED="1604090566878" ID="ID_308525147" MODIFIED="1604091341946" TEXT="If logon is succesful - show user menu">
+<icon BUILTIN="go"/>
+<node CREATED="1604090620751" ID="ID_626991154" MODIFIED="1604090676551" TEXT="1. Add income">
+<node CREATED="1604090984584" ID="ID_1362242485" MODIFIED="1604091166650" TEXT="1-&gt;Is income should be added  with today date? (y/n)"/>
+<node CREATED="1604091168133" ID="ID_1411807192" MODIFIED="1604091212281" TEXT="2-&gt;If n from previous question: Type date"/>
+<node CREATED="1604091213960" ID="ID_345506542" MODIFIED="1604091289874" TEXT="3-&gt;Type here source of income"/>
+<node CREATED="1604091285647" ID="ID_1801320068" MODIFIED="1604091322739" TEXT="4-&gt;Type amount of income"/>
+</node>
+<node CREATED="1604090649808" ID="ID_369327452" MODIFIED="1604090673693" TEXT="2. Add expense">
+<node CREATED="1604091391393" ID="ID_1879562772" MODIFIED="1604091411911" TEXT="1-&gt;Is expense should be added with today date? (y/n)"/>
+<node CREATED="1604091412480" ID="ID_1111912130" MODIFIED="1604091426215" TEXT="2-&gt;If n from previous question: Type date"/>
+<node CREATED="1604091427084" ID="ID_1915188190" MODIFIED="1604091475942" TEXT="3-&gt;Type destination of expense"/>
+<node CREATED="1604091476733" ID="ID_1729714730" MODIFIED="1604091487510" TEXT="4-&gt;Type amount of expence"/>
+</node>
+<node CREATED="1604090678303" ID="ID_1937465353" MODIFIED="1604090755262" TEXT="3. Balance from current month">
+<node CREATED="1604091522416" ID="ID_908466456" MODIFIED="1604091594028">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Show:
+    </p>
+    <p>
+      &#160;First incomes.
+    </p>
+    <p>
+      &#160;Second expenses.
+    </p>
+    <p>
+      &#160;Third difference between upper values.
+    </p>
+    <p>
+      Data from current month.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1604090755682" ID="ID_399658511" MODIFIED="1604090768212" TEXT="4. Balance from previous month">
+<node CREATED="1604091601298" ID="ID_1977942205" MODIFIED="1604091624694" TEXT="Same like point 3 with one different - from previous month"/>
+</node>
+<node CREATED="1604090793461" ID="ID_262346069" MODIFIED="1604090803374" TEXT="5. Balance from custom period of time">
+<node CREATED="1604091638708" ID="ID_172564956" MODIFIED="1604091647998" TEXT="1-&gt;Type first limit date"/>
+<node CREATED="1604091648451" ID="ID_1079885136" MODIFIED="1604091656712" TEXT="2-&gt;Type second limit date"/>
+<node CREATED="1604091671103" ID="ID_1757165276" MODIFIED="1604092335211" TEXT="Show data like point 3 but with specific period of time which was indicated in upper points"/>
+</node>
+<node CREATED="1604090820553" ID="ID_1207639221" MODIFIED="1604090824638" TEXT="6. Change password">
+<node CREATED="1604092354158" ID="ID_44615941" MODIFIED="1604092363335" TEXT="1-&gt;Type new password"/>
+</node>
+<node CREATED="1604090825298" ID="ID_1413226699" MODIFIED="1604090832211" TEXT="9. Logout">
+<node CREATED="1604092367880" ID="ID_1537208111" MODIFIED="1604092372837" TEXT="Show main menu"/>
+</node>
+</node>
+<node CREATED="1604090588675" ID="ID_1878576869" MODIFIED="1604091344811" TEXT="If logon is failed - show main menu">
+<icon BUILTIN="stop"/>
+</node>
+</node>
+<node CREATED="1604090449457" ID="ID_1486333534" MODIFIED="1604090455785" TEXT="2. Type password"/>
+</node>
+<node CREATED="1604090314180" ID="ID_1057128823" MODIFIED="1604090317892" TEXT="2. Register">
+<node CREATED="1604090326413" ID="ID_1922705918" MODIFIED="1604091032561" TEXT="1-&gt;Type name"/>
+<node CREATED="1604090348680" ID="ID_1484876134" MODIFIED="1604091036881" TEXT="2-&gt;Type surname"/>
+<node CREATED="1604090353588" ID="ID_643378963" MODIFIED="1604091041813" TEXT="3-&gt; Type login"/>
+<node CREATED="1604090358626" ID="ID_842343523" MODIFIED="1604091045668" TEXT="4-&gt; Type password"/>
+</node>
+<node CREATED="1604090318543" ID="ID_58849090" MODIFIED="1604090321612" TEXT="9. Exit"/>
+</node>
+<node CREATED="1604092392152" ID="ID_1729064304" MODIFIED="1604092528859" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Notes:
+    </p>
+    <ol>
+      <li>
+        Notification with arrow (&lt;number&gt;-&gt;) means that user have to insert some data.
+      </li>
+      <li>
+        Notification without arrow - only numbers means that user have to choose option which want to use.
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
