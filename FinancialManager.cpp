cd57d477d72364d2949a51dc7d31e732cd095bd1
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2020-11-04 11:07:32 +0100

    Delete unused part of code

--- FinancialManager.cpp
+++ FinancialManager.cpp
@@ -5,8 +5,7 @@ void FinancialManager::addIncome(){
     income.typeDateItemAmountForFinancialMovement();
     income.setIncomeId(1);
     income.setUserId(LOGGED_IN_USER);
-    //income.setDate(500);
-    //income.setItem("Karma dla pieska");
-    //income.setAmount(500);
     income.showAllDataOfFinancialMovement();
+
+    incomes.push_back(income);
 }
